5889793db1fde7c5179f52ff83c85f53f8f57c50
freyakeyser <freyakeyser@gmail.com>
2016-10-11 15:19:33 -0300

    trying to fix shared library setting

--- IBSEM.R
+++ IBSEM.R
@@ -1,5 +1,19 @@
 setwd("C:/Users/keyserf/Documents/Data/salmonIBM/runFiles/parameters/")
 require(Rcpp)
 
-main <- sourceCpp("C:/Users/keyserf/Documents/Data/salmonIBM/main.cpp")
+main <- sourceCpp("C:/Users/keyserf/Documents/Data/salmonIBM/main.cpp") 
+                  
+sourceCpp(
+  //[[Rcpp::export]]
+  'R CMD SHLIB -Wno-write-strings main.cpp')
 
+
+?evalCpp
+evalCpp("1+1", showOutput=TRUE)
+
+writeLines(strsplit(Sys.getenv("PATH"), ";")[[1L]])
+
+install.packages("devtools")
+require(devtools)
+
+?sourceCpp
